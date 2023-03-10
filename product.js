e6a2cee34ba730fedfdce1cf2aaea89e9cac07ec
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-10 12:50:45 +0530

    Update product.js

--- product.js
+++ product.js
@@ -2,43 +2,86 @@
 
 
 
-window.onload = function () {
-    // alert("Window loaded!")
+// window.onload = function () {
+//     // alert("Window loaded!")
 
-    var products = JSON.parse(localStorage.getItem("products"));
+//     var products = JSON.parse(localStorage.getItem("products"));
 
-    var divFromHtml = document.getElementById("screen");
+//     var divFromHtml = document.getElementById("screen");
 
-    var array = [];
+//     var array = [];
 
-    for (var i = 0; i < products.length; i++) {
-        array += `<div><img src="${products[i].image}" alt='proImage' /><br/><h1>${products[i].name}</h1><br/><p>${products[i].price}</p><button onclick='addToCart(${JSON.stringify(products[i])})'>Add to cart</button></div>`
-    }
-    divFromHtml.innerHTML = array;
-}
+//     for (var i = 0; i < products.length; i++) {
+//         array += `<div><img src="${products[i].image}" alt='proImage' /><br/><h1>${products[i].name}</h1><br/><p>${products[i].price}</p><button onclick='addToCart(${JSON.stringify(products[i])})'>Add to cart</button></div>`
+//     }
+//     divFromHtml.innerHTML = array;
+// }
 
 
 
-function addToCart(pro) {
-    var product = JSON.stringify(pro);
-    var dataFromLS = JSON.parse(localStorage.getItem("userData"));
-    var currentUser = JSON.parse(localStorage.getItem("current-user"));
-    if(currentUser){
-        var allUsers = [];
-        for(var i =0; i < dataFromLS.length ; i++ ){
-            if(dataFromLS[i].email === currentUser["current-user-email"] ){
-                var newObj = dataFromLS[i];
-                newObj["cartProducts"] = newObj["cartProducts"] || [];
-                newObj["cartProducts"].push(JSON.parse(product));
-                allUsers.push(newObj)
+// function addToCart(pro) {
+//     var product = JSON.stringify(pro);
+//     var dataFromLS = JSON.parse(localStorage.getItem("userData"));
+//     var currentUser = JSON.parse(localStorage.getItem("current-user"));
+//     if(currentUser){
+//         var allUsers = [];
+//         for(var i =0; i < dataFromLS.length ; i++ ){
+//             if(dataFromLS[i].email === currentUser["current-user-email"] ){
+//                 var newObj = dataFromLS[i];
+//                 newObj["cartProducts"] = newObj["cartProducts"] || [];
+//                 newObj["cartProducts"].push(JSON.parse(product));
+//                 allUsers.push(newObj)
                 
-            } else {
-                allUsers.push(dataFromLS[i])
-            }
-        }
-        localStorage.setItem("userData", JSON.stringify(allUsers));
-        alert("Product added to cart!");
-    } else {
-        alert("Login first to add product into cart!")
+//             } else {
+//                 allUsers.push(dataFromLS[i])
+//             }
+//         }
+//         localStorage.setItem("userData", JSON.stringify(allUsers));
+//         alert("Product added to cart!");
+//     } else {
+//         alert("Login first to add product into cart!")
+//     }
+// }
+
+window.onload = function () {
+    var divFromHtml = document.getElementById("product");
+    var products = JSON.parse(localStorage.getItem("product"));
+  
+    var array = [];
+  
+    for (var i = 0; i < products.length; i++) {
+      // console.log(products[i]);
+      array += `<div><img src="${products[i].image}" alt="productImg" /><h2>${products[i].name}</h2><p>${products[i].price}</p><button onclick='addToCart(${JSON.stringify(products[i])})'>Add to cart</button></div>`;
     }
-}
+  
+    divFromHtml.innerHTML = array;
+  };
+  
+  
+  function addToCart(pro){
+      alert("worked");
+      var product =JSON.stringify(pro);
+      var dataFromLs =JSON.parse(localStorage.getItem("userData"));
+      var currentUser =JSON.parse(localStorage.getItem("currentUser"));
+  
+      if(currentUser){
+          var allUsers =[];
+          for( var i=0; i<dataFromLs.length; i++){
+              if(dataFromLs[i].email === currentUser["current-user-email"]){
+                  var newObj = dataFromLs[i];
+                  newObj["cartProducts"] = newObj["cartProducts"] || [];
+                  newObj["cartProducts"].push(JSON.parse(product));
+                  allUsers.push(newObj);
+              }
+              else{
+                  allUsers.push(dataFromLs[i]);
+              }
+          }
+          // console.log()
+          localStorage.setItem("userData",JSON.stringify(allUsers));
+          alert("Product added to cart");
+      }
+      else{
+          alert("login first to add product into cart");
+      }
+  }
